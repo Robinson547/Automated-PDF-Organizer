fa50303a25355af8956506d699709c3a067507f3
Robinson547 <santiagocompani88@gmail.com>
2025-02-09 22:23:56 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Automated-PDF-Organizer
+# Automated PDF Organizer
+
+This is a Python project for automated pdf organizer. More details will be added soon.
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Automated PDF Organizer
+This is the main script for the automated pdf organizer project.
+"""
+
+def main():
+    print("Welcome to Automated PDF Organizer!")
+
+if __name__ == "__main__":
+    main()
